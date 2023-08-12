f8fbcda7a5833644157fe28a5e21b08b31154da9
Kaleab Fikru <140533941+kenzyflash@users.noreply.github.com>
2023-08-12 01:42:28 -0700

    Full commit
    The full upload of the game

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# number_guesser
+# A Number Guessing Game
+
+This is a simple _HTML_ and _JS_ based number guessing game
+
+### Objective of this Game
+
+This game is designed to test your _javascript_ skill on how to understand the usage of :-
+1. Document Object Model (Dom)
+1. Functions
+1. Conditionals (If statement)
+
+### Usage
+
+To play this game use the following link:
+https://kenzyflash.github.io/number_guesser/
+
+**OR**
+
+You can clone the repository and host it via live server in **Visual Studio Code**
+
+```
+git clone https://github.com/kenzyflash/number_guesser.git
+
+cd number_guesser/
+```
---
+++ app.js
@@ -0,0 +1,24 @@
+// Generate a random number between 1 and 100
+const randomNumber = 59
+
+// Get the necessary HTML elements
+const guessInput = document.getElementById('guessInput');
+const guessButton = document.getElementById('guessButton');
+const message = document.getElementById('message');
+
+// Define a function to check the guessed number
+function checkGuess() {
+    const userGuess = parseInt(guessInput.value);
+    
+    if (Number.isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
+        message.textContent = "Please enter a valid number between 1 and 100.";
+    } else if (userGuess === randomNumber) {
+        message.textContent = "Congratulations! You guessed the correct number!";
+    } else if (userGuess < randomNumber) {
+        message.textContent = "Too low! Try again.";
+    } else if (userGuess > randomNumber) {
+        message.textContent = "Too high! Try again.";
+    }
+}
+
+guessButton.addEventListener('click', checkGuess);
